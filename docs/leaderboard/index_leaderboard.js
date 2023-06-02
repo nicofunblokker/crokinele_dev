e67378a4435d2c0fd3de4d9a1d336ea36fe95e67
nicofunblokker <nicovanblokker@gmail.com>
2023-06-02 09:52:05 +0200

    round and display leaderboard

--- docs/leaderboard/index_leaderboard.js
+++ docs/leaderboard/index_leaderboard.js
@@ -1,57 +1,59 @@
 // Retrieve the CSV data from localStorage
 const csvData = localStorage.getItem("csvData");
 
-const parsedData = csvData ? JSON.parse(csvData) : [];
-
-//const parsedData = [["1","2023-06-01T16:39:51.897Z","1","a","5","B"],["1","2023-06-01T16:39:51.897Z","1","b","10","RB"],["1","2023-06-01T16:39:51.897Z","1","c","20","WS"],["1","2023-06-01T16:39:51.897Z","1","d","50","W"],["1","2023-06-01T16:39:51.897Z","2","a","5"],["1","2023-06-01T16:39:51.897Z","2","b","10"],["1","2023-06-01T16:39:51.897Z","2","c","20"],["1","2023-06-01T16:39:51.897Z","2","d","50"],["1","2023-06-01T16:39:51.897Z","3","a","5"],["1","2023-06-01T16:39:51.897Z","3","b","10"],["1","2023-06-01T16:39:51.897Z","3","c","25"],["1","2023-06-01T16:39:51.897Z","3","d","50"],["1","2023-06-01T16:39:51.897Z","4","a","50"],["1","2023-06-01T16:39:51.897Z","4","b","10"],["1","2023-06-01T16:39:51.897Z","4","c","25"],["1","2023-06-01T16:39:51.897Z","4","d","50"],["2","2023-06-01T16:40:29.161Z","1","blue","5","BRB"],["2","2023-06-01T16:40:29.161Z","1","red","0","BB"],["2","2023-06-01T16:40:29.161Z","1","white","60","W"],["2","2023-06-01T16:40:29.161Z","1","black","90","S"],["2","2023-06-01T16:40:29.161Z","2","blue","5"],["2","2023-06-01T16:40:29.161Z","2","red","60"],["2","2023-06-01T16:40:29.161Z","2","white","0"],["2","2023-06-01T16:40:29.161Z","2","black","5"],["2","2023-06-01T16:40:29.161Z","3","blue","50"],["2","2023-06-01T16:40:29.161Z","3","red","60"],["2","2023-06-01T16:40:29.161Z","3","white","0"],["2","2023-06-01T16:40:29.161Z","3","black","5"],["2","2023-06-01T16:40:29.161Z","4","blue","50"],["2","2023-06-01T16:40:29.161Z","4","red","35"],["2","2023-06-01T16:40:29.161Z","4","white","0"],["2","2023-06-01T16:40:29.161Z","4","black","50"]]
-
-
-// Create an object to store the totals and counts for each player
-const summary = {};
-
-// Iterate over each row in the parsed data
-for (const row of parsedData) {
-  const player = row[3];
-  const score = parseFloat(row[4]); // Corrected index to 5
-
-  // Check if the player already exists in the summary object
-  if (summary.hasOwnProperty(player)) {
-    // If the player exists, update the total and count
-    summary[player].total += score;
-    summary[player].count += 1;
-  } else {
-    // If the player doesn't exist, initialize the total and count
-    summary[player] = { total: score, count: 1 };
-  }
-}
-
 // Create an array to store the summary table rows
 const summaryTable = [];
 
-// Iterate over the players in the summary object
-for (const player in summary) {
-  if (summary.hasOwnProperty(player)) {
-    const total = summary[player].total;
-    const count = summary[player].count;
-    const mean = total / count;
+// Create table headers
+const headers = ["Player", "Total", "Mean"];
 
-    // Create a row array with player, total, and mean values
-    const row = [player, total, mean];
+// Check if csvData exists in localStorage
+if (csvData) {
+  const parsedData = JSON.parse(csvData);
 
-    // Add the row to the summary table array
-    summaryTable.push(row);
+  // Create an object to store the totals and counts for each player
+  const summary = {};
+
+  // Iterate over each row in the parsed data
+  for (const row of parsedData) {
+    const player = row[3];
+    const score = parseFloat(row[4]); // Corrected index to 5
+
+    // Check if the player already exists in the summary object
+    if (summary.hasOwnProperty(player)) {
+      // If the player exists, update the total and count
+      summary[player].total += score;
+      summary[player].count += 1;
+    } else {
+      // If the player doesn't exist, initialize the total and count
+      summary[player] = { total: score, count: 1 };
+    }
   }
-}
 
-// Sort the summary table by mean (descending order)
-summaryTable.sort((a, b) => b[2] - a[2]);
+  // Iterate over the players in the summary object
+  for (const player in summary) {
+    if (summary.hasOwnProperty(player)) {
+      const total = summary[player].total;
+      const count = summary[player].count;
+      const mean = total / count;
+
+      // Create a row array with player, total, and mean values
+      const row = [player, total, mean];
+
+      // Add the row to the summary table array
+      summaryTable.push(row);
+    }
+  }
+
+  // Sort the summary table by mean (descending order)
+  summaryTable.sort((a, b) => b[2] - a[2]);
+}
 
 // Generate HTML table
 const table = document.createElement("table");
 
 // Create table header
 const headerRow = document.createElement("tr");
-const headers = ["Player", "Total", "Mean"];
 for (const header of headers) {
   const th = document.createElement("th");
   th.textContent = header;
@@ -59,13 +61,22 @@ for (const header of headers) {
 }
 table.appendChild(headerRow);
 
-// Create table rows
+// Check if summaryTable has rows or display only the headers
 if (summaryTable.length > 0) {
+  // Create table rows with rounded mean values
   for (const row of summaryTable) {
     const tableRow = document.createElement("tr");
-    for (const cell of row) {
+    for (let i = 0; i < row.length; i++) {
+      const cell = row[i];
       const td = document.createElement("td");
-      td.textContent = cell;
+
+      // Round the mean value to 2 decimal places
+      if (i === 2) {
+        td.textContent = parseFloat(cell).toFixed(2);
+      } else {
+        td.textContent = cell;
+      }
+
       tableRow.appendChild(td);
     }
     table.appendChild(tableRow);
