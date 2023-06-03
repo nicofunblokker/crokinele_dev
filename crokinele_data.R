21588a8f1154a8570f084bc0b56cda9636f70fb0
nicofunblokker <nicovanblokker@gmail.com>
2023-06-03 15:05:39 +0200

    updated downstream scripts

--- crokinele_data.R
+++ crokinele_data.R
@@ -2,5 +2,5 @@ table <- read.csv(
   file = "crokinele_results.csv",
   check.names = F, 
   header = F,
-  col.names = c("Round", "Player", "Score", "Twenties")
+  col.names = c("GameID", "Time", "Round", "Player", "Score", "Twenties")
 )
